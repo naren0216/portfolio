b0f835f515a2539feaf4558cad9fba8d8e9f9c9c
naren0216 <102716737+naren0216@users.noreply.github.com>
2025-02-24 19:06:13 +0530

    Add files via upload

---
+++ portfolio/portfolio_project/portfolio_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
